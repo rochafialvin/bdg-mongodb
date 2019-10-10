970db04d3ee34445c0fe7c534066ddbe219984b7
rochafialvin <rochafi.dev@gmail.com>
2019-10-10 12:14:36 +0700

    First post data

--- api.js
+++ api.js
@@ -10,15 +10,37 @@ const mongodb = require('mongodb')
 // Membuat koneksi ke mongodb
 const MongoClient = mongodb.MongoClient
 
-const URL = "mongodb://127.0.0.1:27717"
+const URL = "mongodb://127.0.0.1:27017"
 const databaseName = 'bdg-mongodb'
 
-MongoClient.connect(URL, {}, (err, client) => {
+MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
     if(err){
         return console.log(err)
     }
 
     const db = client.db(databaseName)
+
+    // GET ALL DATA
+
+    // POST DATA
+    app.post('/users', (req, res) => {
+
+        let {name, role, age} = req.body
+
+        db.collection('users').insertOne({name,role,age})
+            .then((resp) => {
+                res.send({
+                    pesan: "Data berhasil di input",
+                    response: {
+                        insertedData : resp.ops[0]
+                    }
+                })
+            }).catch((err) => {
+                res.send(err)
+            })
+
+
+    })
     
 })
 
