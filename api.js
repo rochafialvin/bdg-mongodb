00cd53d593d0e496883eae638299296b8c6af486
rochafialvin <rochafi.dev@gmail.com>
2019-10-11 09:16:56 +0700

    init data

--- api.js
+++ api.js
@@ -20,7 +20,48 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
 
     const db = client.db(databaseName)
 
+    // INIT DATA
+    app.get('/initdata', (req, res) => {
+        db.collection('users').insertMany([
+            { name: 'Jhonny', age: 28 },
+            { name: 'Deep', age: 38 },
+            { name: 'Bean', age: 19 },
+            { name: 'Dora', age: 22 },
+            { name: 'Marvel', age: 32 },
+            { name: 'Benjamin', age: 32 },
+        ]).then((resp) => {
+            res.send("<h1>Data Init berhasil di tambahkan</h1>")
+
+        }).catch(() => {
+            res.send("Gagal init data")
+            
+        })
+    })
+
+    // HOME
+    app.get('/', (req, res) => {
+        
+        res.send('<h1>API Running di port 2099</h1>')
+    })
+
     // GET ALL DATA
+    app.get('/users', (req, res) => {
+
+        db.collection('users').find({}).toArray()
+            .then((resp) => {
+                // resp = [{}, {}, {}]
+                if(resp.length === 0){
+                   return res.send({message: "Data kosong"})
+                } 
+
+                res.send(resp)
+
+            }).catch((err) => {
+                res.send(err)
+
+            })
+
+    })
 
     // POST DATA
     app.post('/users', (req, res) => {
@@ -38,7 +79,6 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
             }).catch((err) => {
                 res.send(err)
             })
-
 
     })
     
