26d4fce5e9957871603367b5b2cf241a66627a78
rochafialvin <rochafi.dev@gmail.com>
2019-10-14 09:59:08 +0700

    Finish mongodb

--- api.js
+++ api.js
@@ -65,19 +65,25 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
 
     // GET ALL DATA WITH 'QUERY'
 
-    /*
-        1. Kirim pesan error ketika age kosong / tidak di isi data
-        2. Jika data tidak ditemukan maka kirim respon dalam bentuk object yang memiliki propert 'err'
-            templat pesan err = Data dengan umur ... tidak di temukan
-    */
     app.get('/users/many', (req, res) => {
-
+        
         let {age} = req.query
+
+        // Jika user tidak memberikan data pada age
+        if(!age){
+            return res.send({error: "Tolong isi data pada proptery age"})
+        }
 
         age = parseInt(age)
 
         db.collection('users').find({age}).toArray()
             .then((resp) => {
+                // find akan memberikan resp dalam bentuk array
+                // Jika data tidak ditemukan
+                if(resp.length == 0){
+                    return res.send({error: `Data dengan umur ${age} tidak di temukan`})
+                }
+
                 res.send(resp)
 
             }).catch((err) =>{
@@ -88,21 +94,25 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
 
     // GET ONE DATA WITH 'QUERY'
 
-    /*
-        1. Kirim pesan error jika user tidak memberikan salah satu atau kedua data (name, age)
-            template err = Mohon isi data untuk properti 'name', 'age'
-        
-        2. Jika data tidak ditemukan, kirim object dg property 'err'
-            templat err = Tidak dapat menemukan user dengan nama ... dan umur ...
-    */
     app.get('/users/one', (req, res) => {
         // req.query = {name, age}
         let {age, name} = req.query
+
+        // Jika salah satu atau keduanya tidak memiliki data
+        if(!name || !age){
+            return res.send({error: "Mohon isi data untuk property 'name' dan 'age' "})
+        }
 
         age = parseInt(age)
 
         db.collection('users').findOne({ age, name})
             .then((resp) => {
+                // findOne memberikan resp dalam bentuk object
+                // resp = {} / 
+                if(!resp){
+                    return res.send({error: `Tidak dapat menemukan user dg nama ${name} dan umur ${age}`})
+                }
+
                 res.send(resp)
 
             }).catch((err) => {
@@ -115,13 +125,13 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
 
     // POST DATA
 
-    /*
-        1. Kirim pesan error jika name, role , age kosong
-            template err = Tolong isi data 'name', 'role', 'age'
-    */
     app.post('/users', (req, res) => {
 
         let {name, role, age} = req.body
+
+        if(!name || !role || !age){
+            return res.send({error: "Tolong isi data 'name', 'role', 'age'"})
+        }
 
         db.collection('users').insertOne({name,role,age})
             .then((resp) => {
@@ -162,6 +172,27 @@ MongoClient.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true}, (err
     })
 
     // DELETE ONE USER BY AGE
+    app.delete('/users/:umur', (req, res) => {
+        // simpan umur dari user
+        let umur = req.params.umur
+
+        umur = parseInt(umur)
+
+        // gunakan umur sebagai penentu user mana yang akan di hapus
+        db.collection('users').deleteOne({age: umur })
+            .then((resp) => {
+                res.send(resp)
+
+            }).catch((err) => {
+                res.send({
+                    error: err
+                })
+                
+            })
+
+
+
+    })
     
 })
 
